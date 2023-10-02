17b4ffa94024eea1106b537aed084be249282eb7
Borecjeborec1 <atzuki@protonmail.com>
2023-10-02 13:08:04 +0200

    removed lib

--- tests/C++/input/node/lib/types-methods/Boolean-Methods.hh
+++
@@ -1,6 +0,0 @@
-// Ignore imports
-#include <string>
-// Ignore imports end
-
-std::string JS_toString(bool value) { return value ? "true" : "false"; }
-bool JS_valueOf(bool value) { return value ? true : false; }
--- tests/C++/input/objects/lib/types-methods/Boolean-Methods.hh
+++
@@ -1,4 +0,0 @@
-#include <string>
-
-std::string JS_toString(bool value) { return value ? "true" : "false"; }
-bool JS_valueOf(bool value) { return value ? true : false; }
